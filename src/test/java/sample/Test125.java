32a68a15222597c71eed7629204e22a2d78849f6
mastersinselenium <mastersinselenium@gmail.com>
2021-10-07 00:16:32 +0530

    first commit
    first commit
    
    Co-Authored-By: Ramesh <38235+ramesh@users.noreply.github.com>

---
+++ src/test/java/sample/Test125.java
@@ -0,0 +1,42 @@
+package sample;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test125 {
+	
+	@Test
+	void sum() {
+		int a=10;
+		int b=20;
+		System.out.println("This method is for Sum");
+		Assert.assertEquals(30,a+b);
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("This method is for Sub");
+		int a=20;
+		int b=19;
+		Assert.assertEquals(a-b, 1);
+		
+	}
+	@Test
+	public void mul() {
+		System.out.println("This method is for Mul");
+		int a=20;
+		int b=30;
+		Assert.assertEquals(600, a*b);
+	}
+	@Test
+	public void div() {
+		System.out.println("This method is used for div");
+		int a=20;
+		int b=10;
+		Assert.assertEquals(2, a/b);
+	}
+	
+
+}
